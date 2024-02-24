5cdacbe6dc9ac37b7f730540d7d0a63c406e1f0c
Michael Stenta <mike@mstenta.net>
2024-02-24 06:11:52 -0500

    Initial commit: add a Document asset type.

---
+++ README.md
@@ -0,0 +1,11 @@
+# farmOS Document
+
+Provides features for managing documents in [farmOS](https://farmOS.org).
+
+## Maintainers
+
+Current maintainers:
+- [Michael Stenta](https://github.com/mstenta)
+
+This project has been sponsored by:
+- [Farmier](https://farmier.com)
---
+++ src/Plugin/Asset/AssetType/Document.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_document\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the document asset type.
+ *
+ * @AssetType(
+ *   id = "document",
+ *   label = @Translation("Document"),
+ * )
+ */
+class Document extends FarmAssetType {
+
+}
